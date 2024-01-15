4c6ae612a18feb15e70f6f377380145820a117ce
Muhammad Basil <basilmuhammad91@gmail.com>
2024-01-15 15:04:58 +0500

    =in progress

--- src/main.js
+++ src/main.js
@@ -1,25 +1,30 @@
-import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/index.css'
+import './assets/main.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './assets/index.css';
 import store from "./store";
 
-const app = createApp(App)
+// Creating a Vue app instance
+const app = createApp(App);
 
+// Navigation guard to check if the route requires authentication
 router.beforeEach((to, from, next) => {
     if (to.matched.some((record) => record.meta.requiresAuth)) {
-      if (store.getters.isAuthenticated) {
-        next();
-        return;
-      }
-      next("/login");
+        if (store.getters.isAuthenticated) {
+            next(); 
+            return;
+        }
+        next("/login");
     } else {
-      next();
+        next(); 
     }
-  });
+});
 
-app.use(store)
-app.use(router)
-app.mount('#app')
+// Using Vuex store and Vue Router in the Vue app
+app.use(store);
+app.use(router);
+
+// Mounting the Vue app to the DOM element with id 'app'
+app.mount('#app');
--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,36 @@
 import { createStore } from 'vuex'
 import createPersistedState from 'vuex-persistedstate';
 import axios from 'axios'
+
 const store = createStore({
   state: {
+    // Initial state for items and authentication user
     items: [],
     authUser: null
   },
   mutations: {
+    // Mutation to set items in the state
     setItems(state, payload) {
       state.items = payload
     },
+    // Mutation to set a mock user for authentication
     setUser(state) {
       state.authUser = {
-        name: 'Joh Doe',
+        name: 'John Doe',
         address: 'Chatakpur-3, Dhangadhi Kailali',
         phone: '+977 9955221114',
-        email: 'john@exmaple.com',
+        email: 'john@example.com',
         designation: 'Web Developer'
       }
     },
+    // Mutation to log out and reset user and posts
     logOut(state) {
       state.authUser = null
-      state.posts = null
+      state.posts = null  
     }
   },
   actions: {
+    // Action to fetch items from an API
     fetchItems({ commit }) {
       axios
         .get('https://jsonplaceholder.typicode.com/todos')
@@ -43,9 +49,11 @@ const store = createStore({
     }
   },
   getters: {
+    // Getter to check if the user is authenticated
     isAuthenticated: state => !!state.authUser,
   },
   plugins: [
+    // Vuex persisted state plugin for state persistence
     createPersistedState({
       // Configuration options here (if needed)...
     }),
